516032c09017b50c34aec433478abde02205eb14
Joseph Stanford <JosephStanford@msn.com>
2026-05-31 15:09:58 -0500

    Add files via upload

---
+++ about_me.py
@@ -0,0 +1,15 @@
+print("=" * 40)  # Prints a line of 40 equal signs
+print("Student Profile")
+print("=" * 40)
+# Personal details stored as variables
+first_name = "Joseph"
+last_name = "Stanford"
+age = 33
+city = "Birmingham"
+job = "security"
+job_years = "one"
+# my code shown on screen
+print(f"My name is {first_name} {last_name} I am {age} years old and live in {city}.")
+print(f"I work in {job} for {job_years} year")
+print("I am learning code to get a job in coding")
+print("I play video games for fun")
